df3bae7a1a2408e41a5189e9d5fe832a2a892d7b
locnp4 <locnp4@fpt.com.vn>
2023-02-19 13:38:32 +0700

    thêm module quản lý khách hàng

---
+++ DrCleanCare/Models/CustomerViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DrCleanCare.Models
+{
+    public class AddCustomerViewModel
+    {
+        public int CustomerId { get; set; }
+        [Required(ErrorMessage = "Tên khách hàng không được rỗng")]
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public string ShippingAddress { get; set; }
+        public string ShippingContact { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string TaxCode { get; set; }
+        public string Note { get; set; }
+    }
+}
